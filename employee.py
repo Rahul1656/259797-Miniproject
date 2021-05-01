58ad7d70290149da7fe0cae980fc693b2b2769cf
Rahul1656 <rj845854@gmail.com>
2021-05-01 11:06:03 +0530

    Updated code

--- employee.py
+++ employee.py
@@ -1,29 +1,56 @@
-import requests
-
-class Employee:
-    """A sample Employee class"""
-
-    raise_amt = 1.05
-
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-
-    @property
-    def email(self):
-        return '{}.{}@email.com'.format(self.first, self.last)
-
-    @property
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
-
-    def monthly_schedule(self, month):
-        response = requests.get(f'http://company.com/{self.last}/{month}')
-        if response.ok:
-            return response.text
+import random
+print(" ---------------------")
+print("| Rock Paper Scissors |")
+print(" ---------------------")
+listCh = ["R", "P", "S"]
+userScore = 0
+computerScore = 0
+i = 1
+chance = int(input("How many time you want to play (no. of chances): "))
+while i <= chance:
+    computerCh = str(random.choice(listCh))
+    userCh = input("Enter Rock, Paper, Scissors (key to press: R,P,S): ").upper()
+    if userCh == computerCh:
+        print("Tie You Both Entered Same")
+    elif userCh == "R":
+        print("Computer Enter: ", computerCh)
+        if computerCh == "P":
+            print("👉 You lose! Paper covers Rock")
+            computerScore += 1
         else:
-            return 'Bad Response!'
+            print("👉 You win! Rock smashes Scissors")
+            userScore += 1
+    elif userCh == "P":
+        print("Computer Enter: ", computerCh)
+        if computerCh == "S":
+            print("👉 You lose! Scissor cut Paper")
+            computerScore += 1
+        else:
+            print("👉 You win! Paper covers Rock")
+            userScore += 1
+    elif userCh == "S":
+        print("Computer Enter: ", computerCh)
+        if computerCh == "R":
+            print("👉 You lose! Rock smashes Scissors")
+            computerScore += 1
+        else:
+            print("👉 You win! Scissor cut Paper")
+            userScore += 1
+    else:
+        print(":(")
+    print("\n\t******ScoreBoard******")
+    print(f"\t You: {userScore} | Computer: {computerScore}")
+    print("\t**********************")
+    print(f"Game No:[{i}]")
+    print("========================================================")
+    i += 1
+print("\n\n##### Game Over #####")
+print("*******************************************")
+if userScore < computerScore:
+    print(
+        "Sorry You lose the game 😭\ncomputer win the game with score:", computerScore
+    )
+elif userScore == computerScore:
+    print("😅 Game is Tie Play Again 😅")
+else:
+    print("😄 You Win the Game with score:", userScore, "😄")
